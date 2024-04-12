732a7ffc04dc0f8641bf1608fdc2e482f591a50a
Alex <alexandra.berivoe@gmail.com>
2024-04-12 13:27:27 +0200

    First commit

---
+++ app/src/Controller/ControllerTwig.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ControllerTwig extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'number' => $number
+        ];
+
+        return $this->render('index.html.twig', $data);
+    }
+
+    #[Route("/report", name: "report")]
+    public function home(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+
+}
